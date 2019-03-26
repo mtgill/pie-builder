2a91d7713f982da216a1b5272302c637a313e48c
Matt Gill <mtgill90@outlook.com>
2019-03-25 20:28:41 -0500

    prints text to DOM

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('mooooooo');
+const addButton = document.getElementById('addButton');
+const inputIngredient = document.getElementById('inputIngredient');
+
+//print to DOM function 
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+const addIngredient = (e) => {
+    e.preventDefault();
+    const inputText = inputIngredient.value; 
+    printToDom('ingredient-container', inputText);
+    inputIngredient.value = '';
+}
+
+const eventListeners = () => {
+    
+    addButton.addEventListener('click', addIngredient);
+};
+
+const init = () => {
+    eventListeners();
+};
+
+init();
